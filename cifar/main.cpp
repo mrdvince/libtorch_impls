235691e796f8283c29f38b4ecc4a266f9ae55f72
Vince <vincivenv@gmail.com>
2021-07-09 05:10:04 +0300

    fix: Test dataset loading mode

--- cifar/main.cpp
+++ cifar/main.cpp
@@ -2,6 +2,7 @@
 
 #include "cifar10.h"
 #include "model.h"
+#include "trainer.h"
 
 using namespace torch;
 
@@ -15,11 +16,11 @@ int main() {
     auto trainloader = data::make_data_loader<data::samplers::RandomSampler>(
         std::move(train_dataset), 4);
 
-    auto test_dataset = CIFAR10(DATA_DIR, CIFAR10::kTest)
-                            .map(data::transforms::Normalize<>({0.5, 0.5, 0.5}, {0.5, 0.5, 0.5}))
-                            .map(data::transforms::Stack<>());
-    auto testloader = data::make_data_loader<data::samplers::RandomSampler>(
-        std::move(train_dataset), 4);
+    auto test_dataset = CIFAR10(DATA_DIR, CIFAR10::Mode::kTest)
+                            .map(torch::data::transforms::Normalize<>({0.5, 0.5, 0.5}, {0.5, 0.5, 0.5}))
+                            .map(torch::data::transforms::Stack<>());
+    auto testloader = torch::data::make_data_loader<torch::data::samplers::SequentialSampler>(
+        std::move(test_dataset), 4);
 
     std::string classes[10] = {"plane", "car", "bird", "cat",
                                "deer", "dog", "frog", "horse", "ship", "truck"};
@@ -34,4 +35,5 @@ int main() {
     // loss func and optimizer
     nn::CrossEntropyLoss criterion;
     optim::Adam optimizer(model->parameters(), optim::AdamOptions(0.001));
+   
 }
